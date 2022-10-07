436635559ba8099eb1c582a761196da5f6280838
Enting Wu <entingwu@fb.com>
2022-10-06 20:46:24 -0700

    test change

--- src/main/java/com/entingwu/ecommerceseckill/EcommerceSecKillApplication.java
+++ src/main/java/com/entingwu/ecommerceseckill/EcommerceSecKillApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSecKillApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(EcommerceSecKillApplication.class, args);
     }
 
