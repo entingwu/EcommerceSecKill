52d0d9c52202a5a8ba28af5c00d811a4e7941aa2
Enting Wu <entingwu@fb.com>
2022-10-06 20:38:32 -0700

    initial commit

---
+++ src/main/java/com/entingwu/ecommerceseckill/EcommerceSecKillApplication.java
@@ -0,0 +1,13 @@
+package com.entingwu.ecommerceseckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSecKillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceSecKillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/entingwu/ecommerceseckill/EcommerceSecKillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.entingwu.ecommerceseckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSecKillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
